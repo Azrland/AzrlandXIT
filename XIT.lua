7757cf741c79495dc4c31cc37143a11354a1434b
Azrland <azrlandpresent@gmail.com>
2025-03-09 10:11:43 +0700

    Delete XIT.lua

--- XIT.lua
+++
@@ -1,8 +0,0 @@
-local StarterGui = game:GetService("StarterGui")
-StarterGui:SetCore("SendNotification", {
-    Title = "Azrland";
-    Text = "Subscribe Azrland";
-    Duration = 5;
-})
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/username/Roblox-Script-Menu/main/AzrlandXIT.lua", true))()
