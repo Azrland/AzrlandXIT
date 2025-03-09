044af1487616c7eb6a6ad81c17dc30674da16060
Azrland <azrlandpresent@gmail.com>
2025-03-09 10:12:57 +0700

    RBX

---
+++ XIT.lua
@@ -0,0 +1,8 @@
+local StarterGui = game:GetService("StarterGui")
+StarterGui:SetCore("SendNotification", {
+    Title = "Azrland";
+    Text = "Subscribe Azrland";
+    Duration = 5;
+})
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/username/Roblox-Script-Menu/main/AzrlandXIT.lua", true))()
