bc2589fb90255dbf2da607b31a8207c12f1c3de0
Azrland <azrlandpresent@gmail.com>
2025-03-09 10:19:18 +0700

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,4 @@
+-- Load semua script dari GitHub
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Azrland/AzrlandXIT/main/GetKey.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Azrland/AzrlandXIT/main/azrlandxit.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Azrland/AzrlandXIT/main/xit.lua", true))()
