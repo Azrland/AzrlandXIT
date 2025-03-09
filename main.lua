9ef70c607e7981ff396ea7e4a4aae95655c04db0
Azrland <azrlandpresent@gmail.com>
2025-03-09 10:20:00 +0700

    Delete main.lua

--- main.lua
+++
@@ -1,4 +0,0 @@
--- Load semua script dari GitHub
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Azrland/AzrlandXIT/main/GetKey.lua", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Azrland/AzrlandXIT/main/azrlandxit.lua", true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Azrland/AzrlandXIT/main/xit.lua", true))()
